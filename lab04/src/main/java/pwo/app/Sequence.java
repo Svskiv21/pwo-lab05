5dfb2524219bb4d66b216ac4af21cb41e37832aa
Svskiv21 <natalia.krukar@gmail.com>
2022-11-20 19:14:54 +0100

    Dystrybucja 1

--- lab04/src/main/java/pwo/app/Sequence.java
+++
@@ -1,7 +0,0 @@
-package pwo.app;
-
-public class Sequence {
-    public static void main(String[] args) {
-        (new SeqToFileApp()).run(args);
-    }
-}
--- lab04/src/main/java/pwo/utils/SequenceGenerator.java
+++
@@ -1,9 +0,0 @@
-package pwo.utils;
-
-import java.math.BigDecimal;
-
-public interface SequenceGenerator {
-    void reset();
-    BigDecimal nextTerm();
-    BigDecimal getTerm(int i);
-}
